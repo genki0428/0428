ccaf6bdd34ab2d4fb28a737d11fbbd321c2dfa9e
genki0428 <g.lcol.h31@gmail.com>
2024-06-27 04:06:08 +0900

    ルートパス変更

--- peoput/Views/Players/top.php
+++ peoput/Views/Players/top.php
@@ -4,35 +4,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta charset="UTF-8">
     <title>peoput_top</title>
-    <link rel="stylesheet" type="text/css" href="/css/top.css">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/0428/peoput/public/css/top.css">
+    <link rel="stylesheet" type="text/css" href="/0428/peoput/public/css/style.css">
   </head>
   <!-- <header>
     <img src="/img/peoput.png">
   </header> -->
-  <?php include "../Views/Players/header.php"; ?>
+  <?php include "/0428/peoput/public/Views/Players/header.php"; ?>
   <body>
     <div class="top">
-      <img class="hana" src="/img/peoput4.png">
-      <img class="sdgs36" src="/img/sdgs36.png">
-      <img class="sdgs1" src="/img/sdgs1.png">
-      <img class="sdgs19" src="/img/sdgs19.png">
-      <img class="sdgs20" src="/img/sdgs20.png">
-      <img class="sdgs21" src="/img/sdgs21.png">
-      <img class="sdgs22" src="/img/sdgs22.png">
-      <img class="sdgs23" src="/img/sdgs23.png">
-      <img class="sdgs24" src="/img/sdgs24.png">
-      <img class="sdgs25" src="/img/sdgs25.png">
-      <img class="sdgs26" src="/img/sdgs26.png">
-      <img class="sdgs27" src="/img/sdgs27.png">
-      <img class="sdgs28" src="/img/sdgs28.png">
-      <img class="sdgs29" src="/img/sdgs29.png">
-      <img class="sdgs30" src="/img/sdgs30.png">
-      <img class="sdgs31" src="/img/sdgs31.png">
-      <img class="sdgs32" src="/img/sdgs32.png">
-      <img class="sdgs33" src="/img/sdgs33.png">
-      <img class="sdgs34" src="/img/sdgs34.png">
-      <img class="peoput5" src="/img/peoput5.png" style="width: 100%;">
+      <img class="hana" src="/0428/peoput/public/img/peoput4.png">
+      <img class="sdgs36" src="/0428/peoput/public/img/sdgs36.png">
+      <img class="sdgs1" src="/0428/peoput/public/img/sdgs1.png">
+      <img class="sdgs19" src="/0428/peoput/public/img/sdgs19.png">
+      <img class="sdgs20" src="/0428/peoput/public/img/sdgs20.png">
+      <img class="sdgs21" src="/0428/peoput/public/img/sdgs21.png">
+      <img class="sdgs22" src="/0428/peoput/public/img/sdgs22.png">
+      <img class="sdgs23" src="/0428/peoput/public/img/sdgs23.png">
+      <img class="sdgs24" src="/0428/peoput/public/img/sdgs24.png">
+      <img class="sdgs25" src="/0428/peoput/public/img/sdgs25.png">
+      <img class="sdgs26" src="/0428/peoput/public/img/sdgs26.png">
+      <img class="sdgs27" src="/0428/peoput/public/img/sdgs27.png">
+      <img class="sdgs28" src="/0428/peoput/public/img/sdgs28.png">
+      <img class="sdgs29" src="/0428/peoput/public/img/sdgs29.png">
+      <img class="sdgs30" src="/0428/peoput/public/img/sdgs30.png">
+      <img class="sdgs31" src="/0428/peoput/public/img/sdgs31.png">
+      <img class="sdgs32" src="/0428/peoput/public/img/sdgs32.png">
+      <img class="sdgs33" src="/0428/peoput/public/img/sdgs33.png">
+      <img class="sdgs34" src="/0428/peoput/public/img/sdgs34.png">
+      <img class="peoput5" src="/0428/peoput/public/img/peoput5.png" style="width: 100%;">
       <div class="title">
         <h1 style="
         font-size: 1.5rem;
@@ -46,8 +46,8 @@
     </div>
     <div class="a">
       <div class="b">
-        <img class="sdgs2" src="/img/sdgs2.png">
-        <img class="sdgs3" src="/img/sdgs3.png">
+        <img class="sdgs2" src="/0428/peoput/public/img/sdgs2.png">
+        <img class="sdgs3" src="/0428/peoput/public/img/sdgs3.png">
         <div style="margin: 90px 0 0 50px;">
           <h2>Registration</h2>
           <h3 style="margin-left: 225px;">繋がりの登録</h3>
@@ -58,15 +58,15 @@
           <h2>Organize</h2>
           <h3 style="margin-left: 5px;">繋がりを整える</h3>
         </div>
-        <img class="sdgs12" src="/img/sdgs12.png">
-        <img class="sdgs13" src="/img/sdgs13.png">
-        <img class="sdgs14" src="/img/sdgs14.png">
-        <img class="sdgs15" src="/img/sdgs15.png">
+        <img class="sdgs12" src="/0428/peoput/public/img/sdgs12.png">
+        <img class="sdgs13" src="/0428/peoput/public/img/sdgs13.png">
+        <img class="sdgs14" src="/0428/peoput/public/img/sdgs14.png">
+        <img class="sdgs15" src="/0428/peoput/public/img/sdgs15.png">
       </div>
       <div class="d">
-        <img class="sdgs8" src="/img/sdgs8.png">
-        <img class="sdgs9" src="/img/sdgs9.png">
-        <img class="sdgs10" src="/img/sdgs10.png">
+        <img class="sdgs8" src="/0428/peoput/public/img/sdgs8.png">
+        <img class="sdgs9" src="/0428/peoput/public/img/sdgs9.png">
+        <img class="sdgs10" src="/0428/peoput/public/img/sdgs10.png">
         <div style="margin: 105px 0 0 245px;">
           <h2 style="margin-left: 18px;">Search</h2>
           <h3>繋がりを引き出す</h3>
@@ -77,7 +77,7 @@
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-    <script src="/js/top.js"></script>
+    <script src="/0428/peoput/public/js/top.js"></script>
   </body>
-  <?php include "../Views/Players/footer.php"; ?>
+  <?php include "/0428/peoput/public/Views/Players/footer.php"; ?>
 </html>
