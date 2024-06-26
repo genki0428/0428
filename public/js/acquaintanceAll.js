fece3f3ac90deb1cf8ae3c677f8678baadfef709
genki0428 <86515748+genki0428@users.noreply.github.com>
2024-06-27 04:13:57 +0900

    Delete public directory

--- public/js/acquaintanceAll.js
+++
@@ -1,7 +0,0 @@
-function func(eid) {
-  if(confirm("本当に削除しても宜しいですか？")) {
-    location.href ="acquaintanceDel.php?id="+eid;
-  } else {
-    return false;
-  }
-}
--- public/js/myPageAdmin.js
+++
@@ -1,7 +0,0 @@
-function func(eid) {
-  if(confirm("本当に削除しても宜しいですか？")) {
-    location.href ="userDel.php?id="+eid;
-  } else {
-    return false;
-  }
-}
